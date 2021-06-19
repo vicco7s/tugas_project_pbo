413d9317e5e8908158e8344de6248e4eeb634850
Vicco Dwi <50991950+vicco7s@users.noreply.github.com>
2021-06-19 16:58:31 +0800

    class 8 Atribut dan 4 method

---
+++ src/model/Negara.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Negara {
+    //membuat sebuah atribut
+    public String kode_negara;
+    public String nama_negara;
+    public int jumlah_penduduk;
+    public String benua;
+    public String bahasa;
+    public double gdp;
+    public String lagu_kebangsaan;
+    private String ibu_kota;
+
+    //method
+    public void updateInfo(){}
+    public void info(){};
+    public boolean create(){
+        return false;
+    };
+    public boolean delete(){
+        return false;
+    };
+}
