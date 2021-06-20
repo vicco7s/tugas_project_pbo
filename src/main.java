8c38d11f5f78e30f79e239d09845538f148f59fc
Vicco Dwi <50991950+vicco7s@users.noreply.github.com>
2021-06-20 11:38:59 +0800

    class yang berisi object

--- src/main.java
+++ src/main.java
@@ -1,9 +1,12 @@
 import model.Negara;
+import model.Provinsi;
 
 public class main {
     public static void main(String[] args) {
 
-        Negara nr = new Negara(); //memanggil object di dalam class lain
+        Negara nr = new Negara(); //membuat object yg di panggila dari class Negara
+
+        Provinsi pr = new Provinsi(); // membuat object yang di panggil di class provinsi
 
     }
 }
